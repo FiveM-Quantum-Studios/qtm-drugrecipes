4dcb623dce8abc318f9b3adea0d13475e241f568
Tom <111147401+1OSaft@users.noreply.github.com>
2024-07-18 12:15:06 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,5 +1,7 @@
 Config = {}
-
+----------------------------------------------------------------
+Config.checkForUpdates = true
+----------------------------------------------------------------
 Config.PayPhones = {
     'p_phonebox_02_s',
     'prop_phonebox_03',
@@ -8,11 +10,9 @@ Config.PayPhones = {
     'prop_phonebox_04',
     'prop_phonebox_01b'
 }
-
+----------------------------------------------------------------
 Config.Codes = {
-    [12314] = {
-        'You need 2x meth 2x water 3x meow meow'
-    }
+    [12314] = 'You need 2x meth 2x water 3x meow meow'
 }
 
 Config.Language = {
